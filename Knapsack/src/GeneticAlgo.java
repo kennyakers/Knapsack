911a3b77f1009a32315a65d09b66939a97da5417
kennyakers <akers.kenneth66@gmail.com>
2018-10-20 16:23:13 -0700

    Sync

---
+++ Knapsack/src/GeneticAlgo.java
@@ -0,0 +1,13 @@
+/**
+ * Kenny Akers
+ * Mr. Paige
+ * Homework #
+ *
+ */
+public class GeneticAlgo {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ Knapsack/src/Knapsack.java
@@ -0,0 +1,13 @@
+/**
+ * Kenny Akers
+ * Mr. Paige
+ * Homework #
+ *
+ */
+public class Knapsack {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
